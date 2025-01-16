930b3419a7d163199690ee4fdd2b9b0f7b905628
Rimecraftman <151710971+Rimecraftman@users.noreply.github.com>
2025-01-16 10:15:03 -0800

    Lad Fuldon (Dæl 3)

---
+++ fremmweorc/ForþonIcNeCann/lad/tigla/x.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tigla" tilewidth="16" tileheight="16" tilecount="7" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="16" source="C:/Users/grayi/Þencst ðu ic cann/ForþonIcNeCann/lad/tigla/graes.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="C:/Users/grayi/Þencst ðu ic cann/ForþonIcNeCann/lad/tigla/sand.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="C:/Users/grayi/Þencst ðu ic cann/ForþonIcNeCann/lad/tigla/treow.png"/>
+ </tile>
+ <tile id="3">
+  <image width="16" height="16" source="C:/Users/grayi/Þencst ðu ic cann/ForþonIcNeCann/lad/tigla/utofrimum.png"/>
+ </tile>
+ <tile id="4">
+  <image width="16" height="16" source="C:/Users/grayi/Þencst ðu ic cann/ForþonIcNeCann/lad/tigla/waetere.png"/>
+ </tile>
+ <tile id="5">
+  <image width="16" height="16" source="C:/Users/grayi/Þencst ðu ic cann/ForþonIcNeCann/lad/tigla/weall.png"/>
+ </tile>
+ <tile id="6">
+  <image width="16" height="16" source="C:/Users/grayi/Þencst ðu ic cann/ForþonIcNeCann/lad/tigla/wudubord.png"/>
+ </tile>
+</tileset>
